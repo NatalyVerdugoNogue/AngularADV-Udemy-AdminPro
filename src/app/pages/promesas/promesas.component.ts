2be19100b65fa3ccd8a9619e725cb1df32d263b8
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2019-08-08 21:09:50 -0400

    Fin Sección 8

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: []
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() {
+
+    // ==============================================================
+    // Promesa desde constructor sucio
+    // ==============================================================
+
+    // const promesa = new Promise((resolve, reject) => {
+
+    //   let contador = 0;
+
+    //   const intervalo = setInterval(() => {
+
+    //     contador += 1;
+    //     console.log('contador', contador);
+
+
+    //     if (contador === 3) {
+    //       resolve();
+    //       clearInterval(intervalo);
+    //     }
+
+    //   }, 1000);
+    // });
+
+
+    // promesa.then(() =>
+    //   console.log('termino'))
+    //   .catch(error => console.error('Error en promesa', error)
+    //   );
+
+    // ==============================================================
+    // Promesa desde función
+    // ==============================================================
+
+    this.contarTres().then(() =>
+      console.log('termino'))
+      .catch(error => console.error('Error en promesa', error)
+      );
+
+  }
+
+  ngOnInit() {
+  }
+
+  contarTres(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+
+      let contador = 0;
+
+      const intervalo = setInterval(() => {
+
+        contador += 1;
+        console.log('contador', contador);
+
+
+        if (contador === 3) {
+          resolve(true);
+          clearInterval(intervalo);
+        }
+
+      }, 1000);
+    });
+
+  }
+
+}
+
+
